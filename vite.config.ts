fe894b4bcbf4c244750311739af9dbfd3d50c464
Liantsoa Rasata <rmliantsoa@gmail.com>
2025-07-16 15:38:37 +0200

    fix: unit test config

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,25 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   test: {
-    projects: [
-      {
-        test: {
-          name: "unit",
-          environment: "jsdom",
-          globals: true,
-          setupFiles: "./src/tests/setUpTests.ts", // create if not exists
-          include: ["src/**/*.test.{ts,tsx}"],
-        },
-      },
-    ],
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/tests/setUpTests.ts",
+    include: ["src/**/*.test.{ts,tsx}"],
+    exclude: ["node_modules", "storybook-static", "dist", "build"],
+    coverage: {
+      include: ["src/**/*.{ts,tsx}"],
+      exclude: [
+        "src/index.ts",
+        "**/*.d.ts",
+        "storybook-static/**",
+        "**/*.stories.*",
+        "**/manager-bundle.js",
+        "**/runtime.js",
+        "**/globals*.js",
+        "**/common-manager-bundle.js",
+        "**/iframe-*.js",
+        "**/docsRenderer-*.js",
+      ],
+    },
   },
 });
