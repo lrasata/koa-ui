a1433c6adb25856c4b858966e5a30eb58d1b819c
Liantsoa Rasata <rmliantsoa@gmail.com>
2025-08-04 17:54:33 +0200

    chore: fix dialog 1.0.0-rc.12

--- src/components/organisms/Dialog/Dialog.tsx
+++ src/components/organisms/Dialog/Dialog.tsx
@@ -99,7 +99,6 @@ export const Dialog = ({
             ) : (
               <FocusTrap
                 focusTrapOptions={{
-                  onDeactivate: cancel.onClick,
                   escapeDeactivates: true,
                   clickOutsideDeactivates: true,
                 }}
